59fd967049488d15f95c44745b1d0e8ab8bb1aa7
Ion Alfaro <ion.jos.aa@proton.me>
2026-04-26 22:21:45 +0200

    Initial Commit

---
+++ MacMule/MacMuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMuleApp.swift
+//  MacMule
+//
+//  Created by Ion on 26/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MacMuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
